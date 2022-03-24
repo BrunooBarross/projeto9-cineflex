77b78223658d0a6ca4843e23cb714495a8298a55
BrunooBarross <barrosbrunofelipe@gmail.com>
2022-03-23 22:29:13 -0300

    feat: buscando filmes via axios e renderizando na página home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,30 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import "./Home.css"
 const Home = () =>{
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+		const requisicao = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+		requisicao.then(resposta => {
+			setFilmes(resposta.data);
+		});
+	}, []);
+
     return(
-        <div className="home-titulo">
-            <h2>Selecione o filme</h2>
-        </div>
+        <>
+            <div className="home-titulo">
+                <h2>Selecione o filme</h2>
+            </div>
+            <section className='container-filme'>
+                {filmes.map(filme =>             
+                    <div className='home-filme' key={filme.id}>                    
+                        <img className='home-filme-img' src={filme.posterURL} alt={filme.title} />
+                    </div>
+                )}
+            </section>        
+        </>
     );
 }
 
