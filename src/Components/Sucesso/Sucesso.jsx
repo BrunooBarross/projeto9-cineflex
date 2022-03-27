5cc9d93cfacd63ef8804f1d8fd9b4c1344aadfdc
BrunooBarross <barrosbrunofelipe@gmail.com>
2022-03-27 15:43:40 -0300

    feat: styling success component - adding rendering of seats by map

--- src/Components/Sucesso/Sucesso.jsx
+++ src/Components/Sucesso/Sucesso.jsx
@@ -1,17 +1,36 @@
+import { Link } from "react-router-dom";
+import "./style.css"
 const Sucesso = (props) => {
     const {assentos, nomeFilme, dataFilme, horaFilme, nome, cpf} = props.sucesso;
     assentos.sort((a, b) => a-b);
     const formatCpf = cpf.substr(0, 3)+'.'+cpf.substr(3, 3)+'.'+cpf.substr(6, 3)+'-'
          +cpf.substr(9, 2);
     return(
-        <div>
-            {assentos}
-            {nomeFilme}
-            {dataFilme}
-            {horaFilme}
-            {nome}
-            {formatCpf}
-        </div>
+        <section className="container-sucesso">  
+            <div className="mensagem">
+                <span>Pedido feito</span>
+                <span>com sucesso!</span>
+            </div>
+            <div className="sucesso-info">
+                <h2>Filme e sessão</h2>
+                <span>{nomeFilme}</span>
+                <span>{dataFilme} {horaFilme}</span>
+            </div>
+            <div className="sucesso-info">
+                <h2>Ingressos</h2>
+                {assentos.map((assento, key) =>(
+                    <span>Assento: {assento}</span>)
+                )}  
+            </div>
+            <div className="sucesso-info">
+                <h2>Comprador</h2>
+                <span>Nome: {nome}</span>
+                <span>CPF: {formatCpf}</span>
+            </div>
+            <Link className="botao-home" to={"/"}>
+                <button> Voltar para home</button>
+            </Link> 
+        </section>
     );
 
 }
